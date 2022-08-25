31dc240682d83ac6b200beaebec2e4ee47c01f71
DenisV <29409771+DenisVolchek@users.noreply.github.com>
2022-08-25 13:27:01 +0300

    Step 3:   - Add readline-sync package;   - Implement requested logic;

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import readline from "readline-sync";
+
+export default () => {
+    console.log('Welcome to the Brain Games!');
+
+    const userName = readline.question('May I have your name? ');
+
+    console.log(`Hello, ${userName}!`);
+}
